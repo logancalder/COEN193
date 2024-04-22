fe775d8b4f7a28d5e691d8489afcba0957f40c46
Logan Calder <lcalder2022@gmail.com>
2024-04-21 23:14:20 -0700

    version restore

--- dgemm/da.h
+++ dgemm/da.h
@@ -45,12 +45,7 @@ void initializePAPI(int EventSet)
         std::cout << "Adding event: " << events[i] << std::endl;
 
         // Add event to EventSet
-        if (PAPI_add_event(EventSet, PAPI_TOT_CYC) != PAPI_OK)
-        {
-            std::cerr << "PAPI event add failed for event: " << events[i] << std::endl;
-            return;
-        }
-        if (PAPI_add_event(EventSet, PAPI_TOT_INS) != PAPI_OK)
+        if (PAPI_add_event(EventSet, events[i]) != PAPI_OK)
         {
             std::cerr << "PAPI event add failed for event: " << events[i] << std::endl;
             return;
