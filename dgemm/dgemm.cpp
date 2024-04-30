1f33e06622a3ae8348e54ecad9347e40e7bd5293
Logan Calder <lcalder2022@gmail.com>
2024-04-30 11:14:46 -0700

    debug

--- dgemm/dgemm.cpp
+++ dgemm/dgemm.cpp
@@ -72,11 +72,12 @@ int main(int argc, char *argv[])
 
     long long values[getNumEvents()];
     long long averageValues[getNumEvents()];
+
     for (int i; i < getNumEvents(); i++)
     {
-        std::cout << "average " << averageValues[i] << std::endl;
-        std::cout << "val " << values[i] << std::endl;
+        averageValues[i] = values[i] = 0;
     }
+
     int trials = 2; // HOW MANY TO RUN
     std::string fileName = getCurrentDateTimeString();
 
