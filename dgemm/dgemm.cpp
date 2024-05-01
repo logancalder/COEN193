d4eb1e1984044c456c17839ad5d4ae66da670970
Logan Calder <lcalder2022@gmail.com>
2024-05-01 15:45:11 -0700

    fixing bugs

--- dgemm/dgemm.cpp
+++ dgemm/dgemm.cpp
@@ -112,6 +112,7 @@ int main(int argc, char *argv[])
         {
             // Dgemm calculations
 
+            std::cout << j;
             startPAPI(EventSet);
             double *matmulOutput = matmul(A, B, C, ALPHA, BETA, m, n, k);
             stopPAPI(values, EventSet, averageValues, i, n_events);
