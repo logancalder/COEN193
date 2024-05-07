bbfb7313e8ddb85d4b6fda149d15fb555156866f
Logan Calder <lcalder2022@gmail.com>
2024-05-07 13:45:02 -0700

    ||

--- dgemm/matmul.h
+++ dgemm/matmul.h
@@ -8,15 +8,14 @@
 
 double *matmul(double *A, double *B, double *C, int alpha, int beta, int m, int n, int k)
 {
-    int i, j, x;
-#pragma omp parallel for private(i, j, k) shared(A, B, C)
-    for (i = 0; i < m; ++i)
+#pragma omp parallel for collapse(2)
+    for (int i = 0; i < m; ++i)
     {
-        for (j = 0; j < n; ++j)
+        for (int j = 0; j < n; ++j)
         {
-            C[m * j + i] = beta * C[m * j + i];
+            C[m * j + i] *= beta;
 
-            for (x = 0; x < k; ++x)
+            for (int x = 0; x < k; ++x)
             {
                 C[m * j + i] += alpha * A[m * i + x] * B[k * x + j];
             }
@@ -25,5 +24,4 @@ double *matmul(double *A, double *B, double *C, int alpha, int beta, int m, int 
 
     return C;
 }
-
 #endif
